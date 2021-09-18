d6af446496b024f75b5e38262f15c363fb018ba6
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-18 01:19:48 -0500

    initial fetch js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,11 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    fetch(apiUrl).then(function(response) {  //Is it that "then" always plugs in the object returned by the fetch it attaches to? So then enters that into a function parameter and runs the code... but is that all "then" ever does?
+        response.json().then(function(data) {
+            console.log(data);
+        });
+      });
 };
 
 getUserRepos();
