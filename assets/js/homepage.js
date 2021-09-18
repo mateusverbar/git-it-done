a83db43099d3224e5ee3652f809ead3e7e9e1123
Matthew Barros <matthewbarros@Matthews-MacBook-Pro.local>
2021-09-18 12:45:38 -0500

    search-by-user and display data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,11 +1,83 @@
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
     
-    fetch(apiUrl).then(function(response) {  //Is it that "then" always plugs in the object returned by the fetch it attaches to? So then enters that into a function parameter and runs the code... but is that all "then" ever does?
-        response.json().then(function(data) {
-            console.log(data);
+    fetch(apiUrl).then(function(response) { 
+        if (response.ok) {
+            response.json().then(function(data) {
+            displayRepos(data,user);
         });
-      });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    })
+    .catch(function(error) {
+        //notice this `catch()` getting chained to the end of the `.then()' method
+        alert("Unable to connect to GitHub");
+    });
 };
 
-getUserRepos();
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username"); 
+
+var formSubmitHandler = function(blorp) {
+    blorp.preventDefault(); //stops the page from just reloading on submit - default browser behavior
+    //get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value="";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos,searchTerm) {
+
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    repoContainerEl.textContent = ""; //removes old content
+    repoSearchTerm.textContent = searchTerm;
+
+    //loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        //format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        //create container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center"; 
+        //create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        //append to container
+        repoEl.appendChild(titleEl);
+
+        //create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        //check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        //apend to container
+        repoEl.appendChild(statusEl);
+        
+        //appendcontainer to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+debugger;
+
+userFormEl.addEventListener("submit", formSubmitHandler);
